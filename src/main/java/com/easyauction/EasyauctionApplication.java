a8cfbb621776c1c0f6d06035ee2a46bbf776cfd9
promodrathod <promodrathod88@gmail.com>
2025-03-15 03:03:43 +0530

    initial commit

---
+++ src/main/java/com/easyauction/EasyauctionApplication.java
@@ -0,0 +1,13 @@
+package com.easyauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyauctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyauction/EasyauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
